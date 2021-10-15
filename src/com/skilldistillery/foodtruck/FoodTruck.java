2c9db7d940fb5eb744e16265b0882a103d2a8215
johnherrera88 <john.herrera88@gmail.com>
2021-10-15 17:23:29 -0600

    changes

---
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruck {
+
+	//this FoodTruck class has data
+	//including the id which
+	//will be generated in a constructor from static field
+	//that is incremented as each truck is created
+	//no user prompt
+	//toString method
+}
---
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruckApp {
+
+	
+	//scanner and 
+	//menu; name, type of food, rating loops
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
